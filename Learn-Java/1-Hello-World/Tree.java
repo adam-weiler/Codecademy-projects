21d2f119a61b3461e9b6fdf3a06111db84005933
Adam <42845085+adam-weiler@users.noreply.github.com>
2019-11-11 15:36:36 -0500

    Adding Tree project.

---
+++ Learn-Java/1-Hello-World/Tree.java
@@ -0,0 +1,14 @@
+// Define your tree class in this file!
+public class Tree {
+  public static void main(String[] args) {
+    // The main method executes the tasks of the class.
+    System.out.println("Hello! My name is Adam and I'm learning to code in Java.");
+    System.out.println("I'm going to plant a tree now.");
+    System.out.println("   *    ");
+    System.out.println("   **   ");
+    System.out.println("   **   ");
+    System.out.println("  ****  ");
+    System.out.println(" ****** ");   
+    System.out.println("~~~~~~~~");
+  }
+}
